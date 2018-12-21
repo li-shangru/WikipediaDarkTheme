5cc29e2b6245b45c3283fc88b540eaec2c097db5
Shangru(Max) Li <37417901+Anesis@users.noreply.github.com>
2018-12-20 21:36:34 -0500

    Update Wikipedia Dark Mode.js

--- Wikipedia Dark Mode.js
+++ Wikipedia Dark Mode.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wikipedia Dark Mode
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Pure Dark theme for wikipedia.org
 // @author       Shangru Li
 // @match        *://*.wikipedia.org/*
@@ -21,31 +21,30 @@
             allElements[i].style.color = "rgb(255, 255, 255)";
         } else {
             // Set regular text to grey
-            allElements[i].style.color = "rgb(175, 175, 175)";
+            allElements[i].style.color = "rgb(155, 155, 155)";
             // Store current background color to array /backgroundColor/
             // Where backgroundColor[0] is the backgroundColor
             // backgroundColor[1] is backgroundColor's red value
             // backgroundColor[2] is backgroundColor's green value
             // backgroundColor[3] is backgroundColor's blue value
-            var backgroundColor = allElements[i].style.backgroundColor.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/);
+            var backgroundColor = allElements[i].style.background.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/);
             // check for null
             if (backgroundColor==null) {
-                allElements[i].style.background = "rgb(20, 20, 20)";
+                allElements[i].style.background = "rgb(35, 35, 35)";
             } else {
                 // Set new backgroundColor value
-                var r = parseInt(backgroundColor[1]) - 220;
-                var g = parseInt(backgroundColor[2]) - 220;
-                var b = parseInt(backgroundColor[3]) - 220;
+                var r = parseInt(backgroundColor[1]) - 200;
+                var g = parseInt(backgroundColor[2]) - 200;
+                var b = parseInt(backgroundColor[3]) - 200;
                 if (r <= 0 ) {
-                    r = 10;
+                    r = 20;
                 } else if (g <=0 ) {
-                    g = 10;
+                    g = 20;
                 } else if (b <=0 ) {
-                    b = 10;
+                    b = 20;
                 }
                 allElements[i].style.background = 'rgb(' + r + ',' + g + ',' + b + ')';;
             }
         }
     }
 })();
-
